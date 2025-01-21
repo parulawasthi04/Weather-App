6573c699ace4135b1a1814d7b392b02906b2a586
parulawasthi04 <144371278+parulawasthi04@users.noreply.github.com>
2025-01-21 17:20:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelector("button").addEventListener("click", () => {
   }
 
   const promise = fetch(
-    `http://api.weatherapi.com/v1/current.json?key=d07a894153e24c34b6c43352252101&q=${place}&aqi=yes`
+    `https://api.weatherapi.com/v1/current.json?key=d07a894153e24c34b6c43352252101&q=${place}&aqi=yes`
   );
   promise.then((response) => response.json()).then((data) => updateTemp(data));
 });
