21754bd9d4060ff64be3168c0fc828362c3eafa7
parulawasthi04 <144371278+parulawasthi04@users.noreply.github.com>
2025-01-21 12:28:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector("button").addEventListener("click", () => {
+  const place = document.getElementById("location").value;
+
+  function updateTemp(data) {
+    const element = document.getElementById("weatherInfo");
+    element.innerHTML = `Today's temperature is : ${data.current.temp_c}`;
+  }
+
+  const promise = fetch(
+    `http://api.weatherapi.com/v1/current.json?key=d07a894153e24c34b6c43352252101&q=${place}&aqi=yes`
+  );
+  promise.then((response) => response.json()).then((data) => updateTemp(data));
+});
